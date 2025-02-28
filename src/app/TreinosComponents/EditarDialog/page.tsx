cdca33c20fb5ae4d2284e18763c7bf74f7191af4
Rafael <rafagon150456@gmail.com>
2025-02-28 02:36:48 -0300

    update errors deploy

--- src/app/TreinosComponents/EditarDialog/page.tsx
+++ src/app/TreinosComponents/EditarDialog/page.tsx
@@ -14,7 +14,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 
-export function EditarDialog() {
+export default function EditarDialog() {
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
